eb488126e55cd5813e13f823055701210faf3f1f
barathprathosh <barathprathosh@gmail.com>
2020-05-08 15:35:40 +0000

    Script Reports

---
+++ paypro/paypro/report/loan_repayment/loan_repayment.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Teampro and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Loan Repayment"] = {
+	"filters": [
+
+	]
+};
